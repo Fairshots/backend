ec08f33de94b8cfadcb238bd60b0201b91595905
leovcunha <lvcunha@gmail.com>
2018-04-06 20:19:35 +0000

    server basic function

---
+++ app.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logger('dev'));
+
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to Fairshots backend.',
+}));
+
+app.listen(port, (err) => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(`Server running on port: ${ port }`);
+	}
+});
