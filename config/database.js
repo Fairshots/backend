58ee0a71865cdc0f7d019889f73d8ea6e3ae644c
leovcunha <lvcunha@gmail.com>
2018-04-12 18:18:31 +0000

    fixed bug with db sync in production

---
+++ config/database.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: 'hdvdbwhnwqffpv',
+    password: 'b7d56288c3e7387c9f7199e5a689239c10be37891ecbabbb4b12301927cead74',
+    database: 'd1jj1hjho6mdgc',
+    host: 'ec2-23-23-142-5.compute-1.amazonaws.com',
+    port: 5432,
+    dialect: 'postgres',
+    ssl: true,
+    dialectOptions: {
+        ssl: true
+    }
+
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    ssl: true,
+    dialectOptions: {
+        ssl: true
+    }
+  }
+}
