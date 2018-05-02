1782ed1b11e27e01859dc2471d9aa71bff73ff58
leovcunha <lvcunha@gmail.com>
2018-05-02 19:27:49 +0000

    basic testing added

--- config/database.js
+++ config/database.js
@@ -1,4 +1,17 @@
 module.exports = {
+  test: {
+    username: 'hdvdbwhnwqffpv',
+    password: 'b7d56288c3e7387c9f7199e5a689239c10be37891ecbabbb4b12301927cead74',
+    database: 'd1jj1hjho6mdgc',
+    host: 'ec2-23-23-142-5.compute-1.amazonaws.com',
+    port: 5432,
+    dialect: 'postgres',
+    ssl: true,
+    dialectOptions: {
+        ssl: true
+    }
+
+  },
   development: {
     username: 'hdvdbwhnwqffpv',
     password: 'b7d56288c3e7387c9f7199e5a689239c10be37891ecbabbb4b12301927cead74',
