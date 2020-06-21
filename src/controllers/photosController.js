78db59210723ee2de9771c01170c0149a97e54d2
kerry <ksmurph1@gmail.com>
2020-06-21 17:20:26 -0500

    update on photosController is updating

--- src/controllers/photosController.js
+++ src/controllers/photosController.js
@@ -4,34 +4,33 @@ const cloudinary = require('../utilities/cloudinary-funcs');
 module.exports = {
   bulkCreate(req, res) {
     return Photos
-      .bulkCreate(req.body.photos, {returning: true})
+      .bulkCreate(req.body.photos, { returning: true })
       .then(photos => res.status(201).send(photos))
       .catch(error => res.status(400).send(error));
   },
 
-    delete(req, res) {
-      Photos.findByPk(req.body.photoIds[0]).then( photo => {
-        if (photo.cloudlink.includes("cloudinary")) {
-          cloudinary.delPhoto(photo.cloudlink).then(ok => {
-                Photos.destroy({ where: {id: req.body.photoIds } }).then(result =>
-                res.json({ msg: `${result} photo deleted from database successfully` }));
-          })
+  delete(req, res) {
+    Photos.findByPk(req.body.photoIds[0]).then(photo => {
+      if (photo.cloudlink.includes('cloudinary')) {
+        cloudinary.delPhoto(photo.cloudlink).then(ok => {
+          Photos.destroy({ where: { id: req.body.photoIds } }).then(result =>
+            res.json({ msg: `${result} photo deleted from database successfully` }));
+        })
           .catch(error => res.status(500).send(error));
-        } else {
-          Photos.destroy({ where: {id: req.body.photoIds } }).then(result =>
-                res.json({ msg: `${result} photo deleted from database successfully` }))
+      } else {
+        Photos.destroy({ where: { id: req.body.photoIds } }).then(result =>
+          res.json({ msg: `${result} photo deleted from database successfully` }))
           .catch(error => res.status(500).send(error));
-
-        }
-      });
-
+      }
+    });
   },
 
-  update(req,res) {
-    for (var item in req.body)
-    {
-      console.log(item);
-    }
-    res.status(201).send("");
+  update(req, res) {
+    Promise.all(req.body.map((key, _) =>{return Photos.update(
+         {portfolioOrder: key.order },
+        { where: { id: key.id }, fields: ['portfolioOrder'], }
+      );})).then(result => res.status(200).send(result)).catch(
+        err => res.status(501).send(err)
+      );
   }
 };
